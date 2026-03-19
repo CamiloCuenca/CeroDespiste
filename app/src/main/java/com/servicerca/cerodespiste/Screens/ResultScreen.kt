2d9fecce36ef19c30f241a28082847f5e13e2e39
CamiloCuenca <camilocuenca1810@gmail.com>
2026-03-19 18:57:02 -0500

    Add GameScreen and ResultScreen components with preview functionality

---
+++ app/src/main/java/com/servicerca/cerodespiste/Screens/ResultScreen.kt
@@ -0,0 +1,15 @@
+package com.servicerca.cerodespiste.Screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun ResultScreen() {
+
+}
+
+@Composable
+@Preview
+fun ResultScreenPreview() {
+
+}
