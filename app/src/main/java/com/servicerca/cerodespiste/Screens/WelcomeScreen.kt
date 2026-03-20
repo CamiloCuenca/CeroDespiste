a17c8d993375a6d3a1fe92d43a704f95366f471d
CamiloCuenca <camilocuenca1810@gmail.com>
2026-03-19 19:27:50 -0500

    Refactor WelcomeScreen to use Column layout and add preview functionality

--- app/src/main/java/com/servicerca/cerodespiste/Screens/WelcomeScreen.kt
+++ app/src/main/java/com/servicerca/cerodespiste/Screens/WelcomeScreen.kt
@@ -1,9 +1,21 @@
 package com.servicerca.cerodespiste.Screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun WelcomeScreen(){
-    Text(text = "Bienvenido a la aplicación")
+    Column() {
+        Text(text = "Bienvenido a Cero Despite ")
+    }
+}
+
+
+
+@Composable
+@Preview
+fun WelcomeScreenPreview(){
+    WelcomeScreen()
 }
