bb355b6ae0a02cdff077cfbf2016a098b36ceb06
CamiloCuenca <camilocuenca1810@gmail.com>
2026-03-19 19:36:45 -0500

    Enhance WelcomeScreen with background image and improved layout

--- app/src/main/java/com/servicerca/cerodespiste/Screens/WelcomeScreen.kt
+++ app/src/main/java/com/servicerca/cerodespiste/Screens/WelcomeScreen.kt
@@ -1,15 +1,36 @@
 package com.servicerca.cerodespiste.Screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.servicerca.cerodespiste.R
 
 @Composable
 fun WelcomeScreen(){
-    Column() {
-        Text(text = "Bienvenido a Cero Despite ")
+
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        // Imagen de fondo
+        Image(
+            painter = painterResource(id = R.drawable.fondo), // Tu imagen en drawable
+            contentDescription = null, // Fondo decorativo
+            modifier = Modifier.fillMaxSize(),
+            contentScale = ContentScale.Crop // Ajusta para cubrir toda la pantalla
+        )
+
+        Column() {
+            Text(text = "Bienvenido a Cero Despite ")
+        }
     }
+
 }
 
 
